e3fd2aee4c9486e47a46597b25c5d9ad843762c2
Michelle <xaunyunmichelle.lor@digipen.edu>
2025-09-28 02:47:18 +0800

    complete 03-05

--- Ch03-Composite/03_05/begin/drawing-shapes.cpp
+++ Ch03-Composite/03_05/begin/drawing-shapes.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,6 +10,7 @@ class Shape
 {
 public:
     virtual void draw() const = 0;
+    virtual string getName() const = 0;
     virtual ~Shape() = default;
 };
 
@@ -23,6 +25,11 @@ public:
         cout << "Drawing a circle with radius " << m_Radius << endl;
     }
 
+    string getName() const override
+    {
+        return "Circle";
+    }
+
 private:
     double m_Radius;
 };
@@ -35,6 +42,11 @@ public:
     void draw() const override
     {
         cout << "Drawing a rectangle with width " << m_Width << " and height " << m_Height << endl;
+    }
+
+    string getName() const override
+    {
+        return "Rectangle";
     }
 
 private:
@@ -52,23 +64,70 @@ public:
         cout << "Drawing a triangle with sides " << m_Side1 << ", " << m_Side2 << ", and " << m_Side3 << endl;
     }
 
+    string getName() const override
+    {
+        return "Triangle";
+    }
+
 private:
     double m_Side1;
     double m_Side2;
     double m_Side3;
 };
 
+class CompositeShape : public Shape
+{
+private:
+    vector<Shape *> m_Shapes;
+
+public:
+    // Add circle shape
+    void addShape(Shape &shape)
+    {
+        cout << "Adding shape..." << endl;
+        m_Shapes.push_back(&shape);
+    }
+
+    void removeShape(Shape &shape)
+    {
+        cout << "Removing shape..." << endl;
+        Shape *shape_Ptr = &shape;
+        auto it = std::find(m_Shapes.begin(), m_Shapes.end(), shape_Ptr);
+        if (it != m_Shapes.end())
+            m_Shapes.erase(it);
+    }
+
+    void draw() const override
+    {
+        for (const Shape *s : m_Shapes)
+        {
+            s->draw();
+        }
+    }
+
+    string getName() const override
+    {
+        return "Shape Container";
+    }
+};
+
 int main()
 {
-    vector<Shape *> shapes{
-        new Circle(5),
-        new Rectangle(10, 20),
-        new Triangle(3, 4, 5)};
+    Circle c(5);
+    Rectangle r(10, 20);
+    Triangle t(3, 4, 5);
 
-    for (const auto &shape : shapes)
-    {
-        shape->draw();
-    }
+    CompositeShape cs;
+
+    cs.addShape(c);
+    cs.addShape(r);
+    cs.addShape(t);
+
+    cs.draw();
+
+    cs.removeShape(r);
+
+    cs.draw();
 
     return 0;
 }
